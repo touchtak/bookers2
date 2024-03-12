86f605b78d449db5078faa647a0d5e8d12855a22
touchtak <159424108+touchtak@users.noreply.github.com>
2024-03-12 07:39:45 +0000

    26

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to user_path(current_user.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
   end
 
   def edit
   end
 
   def update
+  end
+
+  def index
+    @user = User.all
   end
 end
