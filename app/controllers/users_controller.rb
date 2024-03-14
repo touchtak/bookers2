24f34a18dd20d4f89b3152dd27f5a2309ef3c7a1
touchtak <159424108+touchtak@users.noreply.github.com>
2024-03-14 10:51:07 +0000

    46

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,28 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = current_user
+    if user.update(user_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to user_path(user.id)
+    else
+      @user = current_user
+      render :show
+    end
   end
 
   def index
     @users = User.all
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
